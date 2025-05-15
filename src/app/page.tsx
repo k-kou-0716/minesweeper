459b5440d563ae53b8000b90be0e44102ddc4f22
k-kou-0716 <k.kou.0716@i.softbank.jp>
2025-05-15 13:01:56 +0900

    なんとなく目途が立った

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,85 +1,77 @@
-// 'use client';
+'use client';
 
-// import { useState } from 'react';
-// import styles from './page.module.css';
+import { useState } from 'react';
+import styles from './page.module.css';
 
-// export default function Home() {
-//   const [sampleCounter, setsampleCounter] = useState(0);
-//   const [userInputs, setUserInputs] = useState<number[][]>([
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   ]);
-//   const [bombMap, setBombMap] = useState<number[][]>([
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//   ]);
-//   const calcBoard = (userInputs: number[][], bombMap: number[][]) => {
-//     const newUserInputs = structuredClone(userInputs);
-//     newUserInputs[y][x] += 1;
-//     setUserInputs(userInputs)
-//     return newUserInputs;
-//   };
+export default function Home() {
+  const [sampleCounter, setsampleCounter] = useState(0);
+  const [userInputs, setUserInputs] = useState<number[][]>([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
+  const [bombMap, setBombMap] = useState<number[][]>([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+  ]);
 
-//   const clickHandler = (x: number, y: number) => {
-//     calcBoard(userInputs, bombMap)
-//     };
-//   };
+  //左クリック０（蓋），１（はてな），２（フラグ）～
+  const clickHandler = (x: number, y: number) => {
+    const newUserInputs = structuredClone(userInputs);
+    newUserInputs[y][x] = (newUserInputs[y][x] + 1) % 3;
+    setUserInputs(newUserInputs);
+  };
+  console.log(userInputs);
+  setsampleCounter((sampleCounter + 1) % 14);
 
-//   // const calcUserInputs = (userInputs: number[][]) => {
-//   //   const newUserInputs = structuredClone(userInputs);
-//   //   setsampleCounter((sampleCounter + 1) % 7);
-//   //   setUserInputs(newUserInputs);
-//   // };
+  //右クリック４（開ける）
+  //再帰関数（calcBoard===0なら）
 
-//   // const clickHandler = (x: number, y: number) => {
-//   //   const newUserInputs = structuredClone(userInputs);
-//   //   newUserInputs[x][y] += 1;
-//   //   setUserInputs(newUserInputs);
-//   //   console.log(x, y);
-//   // };
+  //爆弾設置
+  //userInputsに４があったら爆弾を配置
+  if (userInputs.flat().filter((cell) => cell === 4).length === 1) {
+    const newBombMap = structuredClone(bombMap);
+    //ランダム
 
-//   // const clickHandler = (x: number, y: number) => {
-//   //   const newUserInputs = structuredClone(userInputs);
-//   //   setsampleCounter((sampleCounter + 1) % 14);
-//   //   console.log(sampleCounter);
-//   //   console.log(x, y);
-//   //   setUserInputs(newUserInputs);
-//   // };
+    setBombMap(newBombMap);
+  }
 
-//   //装飾の要素がばらばらでclickが反応しない→cssを勉強する
-//   //.mapのところを計算値にする
-//   // <div className={styles.frameTop} />
-//   //       <div className={styles.frameBottom} />
-//   return (
-//     <div className={styles.container}>
-//       <div className={styles.board}>
-//         {calcBoard(userInputs, bombMap).map((row, y) =>
-//           row.map((cell, x) => {
-//             return (
-//               <div key={`${x}-${y}`} className={styles.cell} onClick={() => clickHandler(x, y)}>
-//                 <div
-//                   className={styles.design}
-//                   style={{ backgroundPosition: cell === 30 ? `${-30 * sampleCounter + 30}px` : '' }}
-//                 />
-//               </div>
-//             );
-//           }),
-//         )}
-//       </div>
-//     </div>
-//   );
-// }
+  //周りの爆弾の数
+  // const calcBoard = (userInputs: number[][], bombMap: number[][]) => {};
+
+  //装飾の要素がばらばらでclickが反応しない→cssを勉強する
+  //.mapのところを計算値にする
+  // <div className={styles.frameTop} />
+  //       <div className={styles.frameBottom} />
+  return (
+    <div className={styles.container}>
+      <div className={styles.board}>
+        {userInputs.map((row, y) =>
+          row.map((cell, x) => {
+            return (
+              <div key={`${x}-${y}`} className={styles.cell} onClick={() => clickHandler(x, y)}>
+                <div
+                  className={styles.design}
+                  style={{ backgroundPosition: `${-30 * sampleCounter}px` }}
+                />
+              </div>
+            );
+          }),
+        )}
+      </div>
+    </div>
+  );
+}
